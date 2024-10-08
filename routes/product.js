6d0139f4f87b2aeb74fa48cdc88fc9d3d48a1454
mairoraga <mairoraga@gmail.com>
2024-10-08 16:09:09 +0800

    revision

--- routes/product.js
+++ routes/product.js
@@ -8,7 +8,7 @@ router.get("/all", verify, verifyAdmin, productController.getAllProducts);
 
 router.post("/", verify, verifyAdmin, productController.addProduct);
 
-router.post("/active", productController.getAllACtive);
+router.post("/active", productController.getAllActive);
 
 router.get("/:productid", productController.getProduct);
 
