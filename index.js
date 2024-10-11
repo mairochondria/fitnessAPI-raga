ae205189fe33f185618fac74cd30d9ab52f07325
mairoraga <mairoraga@gmail.com>
2024-10-11 09:13:36 +0800

    revision

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const session = require("express-session");
-const userRoutes = require('./routes/user')
-const productRoutes = require('./routes/product')
-const cartRoutes = require('./routes/cart')
-const orderRoutes = require('./routes/order')
+const userRoutes = require('./routes/user');
+const productRoutes = require('./routes/product');
+const cartRoutes = require('./routes/cart');
+const orderRoutes = require('./routes/order');
 
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // const corsOptions = {
 //     origin: ['http://localhost:8000'],
@@ -21,10 +21,10 @@ app.use(express.urlencoded({extended: true}))
 
 // app.use(cors(corsOptions))
 
-app.use('/b1/users', userRoutes)
-app.use('/b1/products', productRoutes)
-app.use('/b1/cart', cartRoutes)
-app.use('/b1/orders', orderRoutes)
+app.use('/b1/users', userRoutes);
+app.use('/b1/products', productRoutes);
+app.use('/b1/cart', cartRoutes);
+app.use('/b1/orders', orderRoutes);
 
 mongoose.connect(process.env.MONGODB_STRING)
 
