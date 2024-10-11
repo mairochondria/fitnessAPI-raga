c4cd32ef2205bae289be396e8f8b07e7e6c0699d
mairoraga <mairoraga@gmail.com>
2024-10-11 08:44:31 +0800

    revision

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.post("/register", userController.registerUser)
 
 router.get("/details", verify, userController.getDetails)
 
-router.patch('/:id/set-as-admin', verifyToken, verifyAdmin, userController.updateUserToAdmin);
+router.patch('/:id/set-as-admin', verify, verifyToken, verifyAdmin, userController.updateUserToAdmin);
 
 router.patch('/update-password', verify, userController.resetPassword);
 
